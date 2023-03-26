5d31fc68005489dcbeae26bf181708a207eed6de
anishwin <anishwinter17@gmail.com>
2023-03-26 23:43:37 +0530

    My second Commit

--- Driver.js
+++ Driver.js
@@ -3,4 +3,4 @@ import add,{substract,multiply} from "./Reusable.js"
 console.log('Sum of 2 numbers is '+ add(3,5))
 console.log('Difference of 2 numbers is '+ substract(12,5))
 console.log('Product of 2 numbers is '+ multiply(4,12))
-console.log('My name is Anish')
+console.log('My name is Anish ')
